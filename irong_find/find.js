a8b2682e5eadd934fdb74bdcc3d1f189646983ce
周航山 <78966280+zhs141@users.noreply.github.com>
2022-10-23 13:28:55 +0800

    Create find.js

---
+++ irong_find/find.js
@@ -0,0 +1,7 @@
+input.addEventListener('input', (e) => {
+ if (e.target.value === 'irong666666') {
+   window.location.href = 'https://zhs141.github.io/_/irong_find/irong666666.html'
+ } else {
+   window.location.href = 'https://zhs141.github.io/_/irong_find/unirong.html'
+ }
+})
