6b591cc2215965a0f78795164913bd0c6a3dfa81
周航山 <78966280+zhs141@users.noreply.github.com>
2022-10-23 14:03:49 +0800

    Update find.js

--- irong_find/find.js
+++ irong_find/find.js
@@ -1,7 +1,19 @@
 <script>
-var name=prompt('请输入irong编号')
-if (name === 'irong666666') {
-   window.location.href = 'https://zhs141.github.io/_/irong_find/irong666666.html';
+var name=prompt('irong鉴定：请输入您的姓名')
+if (name === '张三') {
+   window.location.href = 'https://zhs141.github.io/_/irong_find/true.html';
+}
+else if (name === '周航山') {
+   window.location.href = 'https://zhs141.github.io/_/irong_find/true.html';
+}
+else if (name === '周秉宏') {
+   window.location.href = 'https://zhs141.github.io/_/irong_find/true.html';
+}
+else if (name === '马伊禧') {
+   window.location.href = 'https://zhs141.github.io/_/irong_find/true.html';
+}
+else if (name === '赵龙麒') {
+   window.location.href = 'https://zhs141.github.io/_/irong_find/true.html';
 }
 else
 {
