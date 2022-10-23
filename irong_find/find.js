03249f734b411fb56c89d8a2427852133c00cc10
周航山 <78966280+zhs141@users.noreply.github.com>
2022-10-23 13:33:04 +0800

    Update find.js

--- irong_find/find.js
+++ irong_find/find.js
@@ -1,4 +1,4 @@
-input.addEventListener('input', (e) => {
+input.addEventListener('请输入irong编号：', (e) => {
  if (e.target.value === 'irong666666') {
    window.location.href = 'https://zhs141.github.io/_/irong_find/irong666666.html'
  } else {
