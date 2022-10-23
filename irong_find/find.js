a4572c8d062493be770a0041a8023449c5ec1283
周航山 <78966280+zhs141@users.noreply.github.com>
2022-10-23 13:43:25 +0800

    Update find.js

--- irong_find/find.js
+++ irong_find/find.js
@@ -1,8 +1,10 @@
 <script>
 var name=prompt('请输入irong编号')
 if (name === 'irong666666') {
-   window.location.href = 'https://zhs141.github.io/_/irong_find/irong666666.html'
- } else {
-   window.location.href = 'https://zhs141.github.io/_/irong_find/unirong.html'
- }
-})
+   window.location.href = 'https://zhs141.github.io/_/irong_find/irong666666.html';
+}
+else
+{
+   window.location.href = 'https://zhs141.github.io/_/irong_find/unirong.html';
+}
+</script>
